25d2d548872bbd586c8c43747917da2373c121ab
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-27 17:56:48 -0300

    test: tested encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("🦋🦄🐝🦆🐝🦄", 3) == "🐝🦄🦋_🦄🐝🦆"
+    assert encrypt_message("🦋🦄🐝🦆🐝🦄", 4) == "🦄🐝_🦆🐝🦄🦋"
+    assert encrypt_message("🦋🦄🐝🦆🐝🦄", 9) == "🦄🐝🦆🐝🦄🦋"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("🦋🦄🐝🦆🐝🦄", "🦋🦄🐝🦆🐝🦄")  # type: ignore
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)  # type: ignore
