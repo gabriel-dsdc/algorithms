e052a9910801e8311424ff2556eea038cc9a5c52
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-27 16:22:57 -0300

    feat: check study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
-def study_schedule(permanence_period, target_time):
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
+    if type(target_time) != int or any(
+        [
+            type(period[0]) != int or type(period[1]) != int
+            for period in permanence_period
+        ]
+    ):
+        return None
+    return len(
+        [
+            period
+            for period in permanence_period
+            if period[0] <= target_time <= period[1]
+        ]
+    )
