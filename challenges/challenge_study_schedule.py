fd7c847d527fe9a8668747c2c9f6ac188b51fd50
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-27 20:19:04 -0300

    refactor: study_schedule more clean and readable

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,14 @@
-def study_schedule(permanence_period: list[tuple[int, int]], target_time: int):
-    if type(target_time) != int or any(
-        [
-            type(period[0]) != int or type(period[1]) != int
-            for period in permanence_period
-        ]
-    ):
+def study_schedule(
+    permanence_period: list[tuple[int, int]], target_time: int
+) -> int | None:
+    if type(target_time) != int:
         return None
-    return len(
-        [
-            period
-            for period in permanence_period
-            if period[0] <= target_time <= period[1]
-        ]
-    )
+
+    counter = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            counter += 1
+    return counter
