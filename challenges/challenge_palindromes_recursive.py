44ee669061548efff048fc03ea3f3ccd0505f8b4
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-02-28 00:16:00 -0300

    feat: palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(
+    word: str, low_index: int, high_index: int
+) -> bool:
+    is_letter_equal = bool(word)
+    if low_index <= high_index:
+        is_letter_equal = word[low_index] == word[high_index]
+        if is_letter_equal:
+            return is_palindrome_recursive(word, low_index+1, high_index-1)
+    return is_letter_equal
