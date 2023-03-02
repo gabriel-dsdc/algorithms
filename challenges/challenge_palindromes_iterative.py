2d02fb1f6cd8c0f4316fb1a6de50af2e82774011
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-02 02:28:11 -0300

    feat: palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str) -> bool:
+    if not word:
+        return False
+
+    for i in range(1, len(word) + 1):
+        if word[i - 1] != word[-i]:
+            return False
+
+    return True
