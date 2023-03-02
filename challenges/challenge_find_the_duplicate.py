8571ba377b92036a3f4c8ebc58606bd52c2c0439
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2023-03-01 22:50:50 -0300

    feat: find_duplicate with binary search

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,32 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+from typing import Literal
+
+
+def binary_search(lst: list, target) -> int:
+    start = 0
+    end = len(lst) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if lst[mid] == target:
+            return mid
+
+        if target < lst[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+def find_duplicate(nums: list[int]) -> int | Literal[False]:
+    nums.sort()
+    end = len(nums) - 1
+    for num in nums:
+        if type(num) != int or num < 1:
+            return False
+
+        num_index = binary_search(nums, num)
+        if num_index < end:
+            if num == nums[num_index + 1] or num == nums[num_index - 1]:
+                return num
+    return False
